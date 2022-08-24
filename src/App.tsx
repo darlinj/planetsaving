6fd7a8fb20dacdd84ee9b23332990ff45de4501a
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-08-24 09:24:26 +0100

    Adding different color cards

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,16 @@ interface ActionData {
   actionTitle: string;
   cost: number;
   carbonSaved: number;
-  actionType: "fuel" | "food";
+  actionType: "energy" | "food" | "transport" | "government" | "purchasing";
 }
+
+const categoryColorMap = {
+  energy: yellow,
+  transport: orange,
+  food: blue,
+  government: green,
+  purchasing: red,
+};
 
 const data: ClimateData[] = [
   {
@@ -40,9 +48,9 @@ const data: ClimateData[] = [
     amount: 2.4,
     subSection: null,
     color: orange[500],
-    label: "Transport(2.4T)",
+    label: "Transport",
   },
-  {amount: 2, subSection: null, color: yellow[500], label: "Fuel"},
+  {amount: 2, subSection: null, color: yellow[500], label: "Energy"},
   {
     amount: 1.1,
     subSection: null,
@@ -57,13 +65,25 @@ const actions: ActionData[] = [
     actionTitle: "Reduce your thermostat by one degree",
     cost: 0,
     carbonSaved: 0.3,
-    actionType: "fuel",
+    actionType: "energy",
   },
   {
     actionTitle: "Buy an electric car",
     cost: 30000,
     carbonSaved: 1.0,
-    actionType: "fuel",
+    actionType: "transport",
+  },
+  {
+    actionTitle: "Stop buying air freighted food",
+    cost: 0,
+    carbonSaved: 0.3,
+    actionType: "food",
+  },
+  {
+    actionTitle: "Buy more second hand things",
+    cost: 0,
+    carbonSaved: 0.4,
+    actionType: "purchasing",
   },
 ];
 
@@ -76,7 +96,12 @@ function App() {
         </Typography>
       </Grid>
       <Grid item xs={8} sx={{minWidth: 440}}>
-        <Paper sx={{padding: 1}}>
+        <Paper
+          sx={{
+            padding: 1,
+            justifyContent: "center",
+          }}
+        >
           <Footprint data={data} />
           <Typography variant="h4" component="div" gutterBottom>
             Total 10.6 Tons of CO2 equivalent
@@ -97,7 +122,7 @@ const ActionsList = ({actionData}: {actionData: ActionData[]}) => {
         paddingLeft: 2,
         paddingRight: 2,
         paddingBottom: 2,
-        minWidth: 300,
+        minWidth: 280,
       }}
     >
       <Typography variant="h4" component="div" gutterBottom align="center">
@@ -111,23 +136,27 @@ const ActionsList = ({actionData}: {actionData: ActionData[]}) => {
 };
 
 const SuggestedActionCard = ({action}: {action: ActionData}) => {
+  const colorArray = categoryColorMap[action.actionType];
   return (
     <Card
       sx={{
         borderRadius: "16px",
-        backgroundColor: yellow[500],
+        backgroundColor: colorArray[500],
         marginBottom: 2,
       }}
     >
       <CardActionArea>
         <CardHeader
           avatar={
-            <Avatar aria-label="recipe">
+            <Avatar
+              aria-label="recipe"
+              sx={{color: colorArray[800], backgroundColor: colorArray[300]}}
+            >
               <LocalFireDepartmentIcon />
             </Avatar>
           }
           action={
-            <IconButton aria-label="settings">
+            <IconButton aria-label="settings" sx={{color: colorArray[800]}}>
               <FavoriteIcon />
             </IconButton>
           }
@@ -136,17 +165,27 @@ const SuggestedActionCard = ({action}: {action: ActionData}) => {
         />
         <CardContent sx={{paddingTop: 0}}>
           <Box display={"flex"}>
-            <ImpactWell text={`Cost=£${action.cost}`} />
-            <ImpactWell text={`CO2 saved=${action.carbonSaved}T`} />
+            <ImpactWell color={colorArray[800]} text={`Cost=£${action.cost}`} />
+            <ImpactWell
+              color={colorArray[800]}
+              text={`CO2 saved=${action.carbonSaved}T`}
+            />
           </Box>
-          <Typography variant="button">Learn More</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="button">Learn More</Typography>
+          </Box>
         </CardContent>
       </CardActionArea>
     </Card>
   );
 };
 
-const ImpactWell = ({text}: {text: string}) => {
+const ImpactWell = ({text, color}: {text: string; color: string}) => {
   return (
     <Box
       sx={{
@@ -155,7 +194,7 @@ const ImpactWell = ({text}: {text: string}) => {
         display: "flex",
         justifyContent: "center",
         width: 100,
-        backgroundColor: yellow[800],
+        backgroundColor: color,
       }}
     >
       <Typography
@@ -179,63 +218,66 @@ const Footprint = ({data}: {data: ClimateData[]}) => {
     .map((item) => item.amount)
     .reduce((acc, val) => acc + val, 0);
   return (
-    <svg width="500" height="500">
-      {data.map((stripe, index) => {
-        const stripeHeight = (stripe.amount / totalAmount) * footHeight;
-        stripeOffset += lastStripeHeight;
-        const midStripe = stripeOffset + stripeHeight / 2;
-        lastStripeHeight = stripeHeight;
-        // const greens = Object.values(green);
-        return (
-          <g key={index}>
-            <rect
-              clipPath="url('#foot')"
-              width="500"
-              y={stripeOffset}
-              height={stripeHeight}
-              style={{
-                fill: stripe.color,
-              }}
-            />
-            <text x="0" y={midStripe} fill="black">
-              {stripe.label}
-            </text>
-            <text x="0" y={midStripe + 20} fill="black">
-              ({stripe.amount} Tons)
-            </text>
-          </g>
-        );
-      })}
-      <clipPath id="clip1">
-        <circle
-          cx="100"
-          cy="100"
-          r="40"
-          stroke="black"
-          strokeWidth="3"
-          fill="none"
-        />
-      </clipPath>
-      <clipPath id="foot">
-        <path
-          d="M224.231,239.9c43.9,23.3,64.5,75.4,49.8,123.3c-2.7,8.6-5.1,17.5-7,26.4c-7.8,40,2.7,90.2,46.7,99.1
+    <Box
+      sx={{display: "flex", alignContent: "center", justifyContent: "center"}}
+    >
+      <svg width="450" height="500">
+        {data.map((stripe, index) => {
+          const stripeHeight = (stripe.amount / totalAmount) * footHeight;
+          stripeOffset += lastStripeHeight;
+          const midStripe = stripeOffset + stripeHeight / 2;
+          lastStripeHeight = stripeHeight;
+          return (
+            <g key={index}>
+              <rect
+                clipPath="url('#foot')"
+                width="500"
+                y={stripeOffset}
+                height={stripeHeight}
+                style={{
+                  fill: stripe.color,
+                }}
+              />
+              <text x="0" y={midStripe} fill="black">
+                {stripe.label}
+              </text>
+              <text x="0" y={midStripe + 20} fill="black">
+                ({stripe.amount} Tons)
+              </text>
+            </g>
+          );
+        })}
+        <clipPath id="clip1">
+          <circle
+            cx="100"
+            cy="100"
+            r="40"
+            stroke="black"
+            strokeWidth="3"
+            fill="none"
+          />
+        </clipPath>
+        <clipPath id="foot">
+          <path
+            d="M224.231,239.9c43.9,23.3,64.5,75.4,49.8,123.3c-2.7,8.6-5.1,17.5-7,26.4c-7.8,40,2.7,90.2,46.7,99.1
 				c22.2,4.7,44.3-3.9,59.9-20.2c46.5-50.5,32.3-202.6,31.9-240.7c0-16.7-0.4-33.8-7-49.4c-14.8-35-56.4-49-93.3-58.7
 				c-60.6-22.1-98.8,5.8-111.6,26.4S171.531,212.5,224.231,239.9z"
-        />
-        <ellipse cx="181.831" cy="44.3" rx="31.5" ry="44.3" />
-        <ellipse cx="254.631" cy="51.7" rx="18.7" ry="26.4" />
-        <ellipse cx="312.931" cy="68" rx="16.7" ry="23.3" />
-        <ellipse cx="366.131" cy="81.7" rx="14.8" ry="20.6" />
-        <ellipse
-          transform="matrix(-0.2014 0.9795 -0.9795 -0.2014 586.9331 -227.4809)"
-          cx="366.2"
-          cy="109.218"
-          rx="18.3"
-          ry="13.2"
-        />
-      </clipPath>
-      Sorry, your browser does not support inline SVG.
-    </svg>
+          />
+          <ellipse cx="181.831" cy="44.3" rx="31.5" ry="44.3" />
+          <ellipse cx="254.631" cy="51.7" rx="18.7" ry="26.4" />
+          <ellipse cx="312.931" cy="68" rx="16.7" ry="23.3" />
+          <ellipse cx="366.131" cy="81.7" rx="14.8" ry="20.6" />
+          <ellipse
+            transform="matrix(-0.2014 0.9795 -0.9795 -0.2014 586.9331 -227.4809)"
+            cx="366.2"
+            cy="109.218"
+            rx="18.3"
+            ry="13.2"
+          />
+        </clipPath>
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    </Box>
   );
 };
 
