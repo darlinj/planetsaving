41102ce773c42905f5f63b2c1500d52e3fb1ad20
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-09-02 16:00:35 +0100

    Remove commented out code

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -35,28 +35,3 @@ resource "aws_s3_bucket_website_configuration" "website-config" {
     key = "404.jpeg"
   }
 }
-
-# resource "aws_s3_bucket" "root_bucket" {
-#   bucket = var.bucket_name
-#   tags = var.common_tags
-# }
-
-# data "aws_s3_bucket" "redirect_bucket" {
-#   bucket = aws_s3_bucket.root_bucket.bucket
-# }
-# resource "aws_s3_bucket_acl" "redirect-bucket-acl" {
-#   bucket = data.aws_s3_bucket.redirect_bucket.id
-#   acl    = "public-read"
-# }
-
-# resource "aws_s3_bucket_policy" "redirect-bucket-policy" {
-#   bucket = data.aws_s3_bucket.redirect_bucket.id
-#   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-# }
-
-# resource "aws_s3_bucket_website_configuration" "redirect-bucket-website-config" {
-#   bucket = data.aws_s3_bucket.redirect_bucket.id
-#   redirect_all_requests_to {
-#     host_name = "www.${var.domain_name}"
-#   }
-# }
