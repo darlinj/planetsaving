a9c09b311da509afadefed1f5e20a206bc399db7
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-10-05 18:14:28 +0100

    Reworking test

--- src/components/Footprint.test.tsx
+++ src/components/Footprint.test.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import Footprint from "./Footprint";
+import useFootprintData from "../api/useFootprintData";
+import {UseQueryResult} from "@tanstack/react-query";
+import {ClimateData} from "../types";
 
-jest.mock("../api/useFootprintData", () => () => {
+jest.mock("../api/useFootprintData");
+
+test("renders the foot", () => {
+  const mockUseFootprintData = useFootprintData as jest.MockedFunction<
+    typeof useFootprintData
+  >;
   const climateData = [
     {label: "Item 1", amount: 3, color: "#000000", subSection: null},
   ];
-  return {status: "success", data: climateData};
-});
-
-test("renders the foot", () => {
+  mockUseFootprintData.mockImplementation(() => {
+    return {
+      status: "success",
+      data: climateData,
+      isFetching: false,
+      isLoading: false,
+    } as UseQueryResult<[ClimateData]>;
+  });
   render(<Footprint />);
   const linkElement = screen.getByText(/Item 1/i);
   expect(linkElement).toBeInTheDocument();
