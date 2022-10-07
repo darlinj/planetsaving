cbf2fd132357e1047c9ced9c2bdb4cb61a4a9750
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-10-07 18:08:10 +0100

    Improve API test

--- backend/integration.test.js
+++ backend/integration.test.js
@@ -47,29 +47,29 @@ const uploadTestData = async (testData) => {
   await new Promise((r) => setTimeout(r, 2000));
 };
 
-describe("Checking what we get", () => {
+describe("the climateChange API", () => {
   beforeEach(async () => {
     await emptyTable();
   });
-  test("some test", async () => {
-    await uploadTestData([
-      {
-        id: 123,
-        label: "Food",
-        color: "Red",
-        amount: 10,
-      },
-      {
-        id: 124,
-        label: "Food",
-        color: "Red",
-        amount: 10,
-      },
-    ]);
+  test("It returns the expected data", async () => {
+    const climateCategory1 = {
+      id: 123,
+      label: "Food",
+      color: "Red",
+      amount: 10,
+    };
+    const climateCategory2 = {
+      id: 124,
+      label: "consumables",
+      color: "Green",
+      amount: 20,
+    };
+    await uploadTestData([climateCategory1, climateCategory2]);
     const result = await server.executeOperation({
-      query: "query { getClimateData { id, label} }",
+      query: "query { getClimateData { id label color amount} }",
     });
 
     expect(result.data.getClimateData.length).toEqual(2);
+    expect(result.data.getClimateData[0]).toEqual(climateCategory1);
   });
 });
