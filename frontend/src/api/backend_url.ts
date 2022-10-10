649e6a20877bd6fc3999b6a6a1af562a44a85002
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-10-10 15:27:39 +0100

    change over to sed as jq isn't on the image

--- frontend/src/api/backend_url.ts
+++ frontend/src/api/backend_url.ts
@@ -1,4 +1,5 @@
 import urlData from "./url.json";
 
-const backendUrl = urlData.url ? urlData.url : "http://localhost:4000";
+const backendUrl =
+  urlData.url == "PLACEHOLDER" ? "http://localhost:4000" : urlData.url;
 export default backendUrl;
