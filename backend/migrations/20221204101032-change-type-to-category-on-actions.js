d1f1c2490ef6d26f2216bd8e2bbf04b2d63123de
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-12-04 15:27:09 +0000

    Fix database types

--- backend/migrations/20221204101032-change-type-to-category-on-actions.js
+++ backend/migrations/20221204101032-change-type-to-category-on-actions.js
@@ -3,12 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Actions", "category", {type: "string"});
+    await queryInterface.addColumn("Actions", "category", {
+      type: Sequelize.STRING,
+    });
     await queryInterface.removeColumn("Actions", "type");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Actions", "type", {type: "string"});
+    await queryInterface.addColumn("Actions", "type", {type: Sequelize.STRING});
     await queryInterface.removeColumn("Actions", "category");
   },
 };
