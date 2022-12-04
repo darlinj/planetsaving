844bcaabc902af881ef1cd64146826b88cf9ab43
Joe Darling <joe.darling@cambridgeconsultants.com>
2022-12-04 17:35:24 +0000

    Fix database types - again

--- backend/migrations/20221203195333-change_color_to_category.js
+++ backend/migrations/20221203195333-change_color_to_category.js
@@ -3,12 +3,16 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("ClimateData", "category", {type: "string"});
+    await queryInterface.addColumn("ClimateData", "category", {
+      type: Sequelize.STRING,
+    });
     await queryInterface.removeColumn("ClimateData", "color");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn("ClimateData", "color", {type: "string"});
+    await queryInterface.addColumn("ClimateData", "color", {
+      type: Sequelize.STRING,
+    });
     await queryInterface.removeColumn("ClimateData", "category");
   },
 };
