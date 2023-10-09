ac4b112ae2d1c342d0e9f13efa76e9540e3f99b8
Joe Darling <joe.darling@cambridgeconsultants.com>
2023-10-09 11:14:15 +0100

    Add default reference

--- backend/resolvers/emissionsCalculators/index.js
+++ backend/resolvers/emissionsCalculators/index.js
@@ -38,7 +38,12 @@ const calculateEmission = (calculationIdentifier, operands) => {
       return {
         calculation: operands.totalCarbonEmited * 1.0,
         description: `${operands.totalCarbonEmited} Tons emitted by ${operands.label}`,
-        referenceUrls: [],
+        referenceUrls: [
+          {
+            label: "Carbon Independent",
+            url: "https://www.carbonindependent.org/",
+          },
+        ],
       };
   }
 };
