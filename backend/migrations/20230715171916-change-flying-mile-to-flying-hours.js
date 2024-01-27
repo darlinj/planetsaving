44fa4f3837896b600d6396a3d9b8545b129d45d8
Joe Darling <joe.darling@cambridgeconsultants.com>
2024-01-27 11:27:33 +0000

    Fix migration

--- backend/migrations/20230715171916-change-flying-mile-to-flying-hours.js
+++ backend/migrations/20230715171916-change-flying-mile-to-flying-hours.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Users", "flyingHoursPerYear", "category", {
+    await queryInterface.addColumn("Users", "flyingHoursPerYear", {
       type: Sequelize.FLOAT,
     });
     await queryInterface.removeColumn("Users", "flyingMilesPerYear");
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Users", "flyingMilesPerYear", "category", {
+    await queryInterface.addColumn("Users", "flyingMilesPerYear", {
       type: Sequelize.INTEGER,
     });
     await queryInterface.removeColumn("Users", "flyingHoursPerYear");
